afa767d17a262e87a3fba25f6516aedd9db5f3b6
DavideViolante <fungtq3@gmail.com>
2014-03-22 15:40:51 +0100

    ex3 almost done

--- 2014-03-21/python/exercise3.py
+++ 2014-03-21/python/exercise3.py
@@ -13,11 +13,11 @@ OCRA1 = rgb([160, 110, 0.0])
 OCRA2 = rgb([180, 130, 0.0])
 OCRA3 = rgb([200, 150, 0.0])
 OCRA3A = rgb([220, 170, 0.0])
-OCRA4 = rgb([240, 190, 0.0])
-OCRA5 = rgb([255, 220, 80])
-OCRA6 = rgb([255, 240, 100])
-OCRA7 = rgb([255, 255, 130])
-OCRA8 = rgb([255, 255, 160])
+OCRA4 = rgb([230, 190, 0.0])
+OCRA5 = rgb([240, 210, 50])
+OCRA6 = rgb([240, 210, 80])
+OCRA7 = rgb([240, 220, 110])
+OCRA8 = rgb([255, 220, 140])
 
 #def heights of the floors
 H1 = 1
@@ -48,7 +48,7 @@ floor3base = COLOR(OCRA3)(JOIN(AA(MK)(pts3)))
 floor3base = T([1,2,3])([6,16,H3])(floor3base)
 
 #column
-column = COLOR(OCRA3A)(JOIN(CIRCUMFERENCE(0.8)(10)))
+column = COLOR(OCRA3A)(JOIN(CYLINDER([0.8,H4-H3])(10)))
 column = T([1,2,3])([7,17,H3col])(column)
 
 #cloning and positioning columns
@@ -56,18 +56,18 @@ columns_x = STRUCT([column, T(1)(3.28)]*12)
 columns_y = STRUCT([column, T(2)(3.2)]*6)
 columns_xcopy = STRUCT([columns_x, T(2)(16)]*2)
 columns_ycopy = STRUCT([columns_y, T(1)(36.08)]*2)
-column2fronta = T([1,2])([5.5,6])(column)
-column2frontb = T([1,2])([5.5,9.5])(column)
+column2fronta = T([1,2])([6,6])(column)
+column2frontb = T([1,2])([6,9.5])(column)
 columns_front = STRUCT([column2fronta, column2frontb])
-columns_rear = STRUCT([columns_front, T(1)(22)]*2)
+columns_rear = STRUCT([columns_front, T(1)(21)]*2)
 
 columnsTOT = STRUCT([columns_x, columns_y, columns_xcopy, columns_ycopy, columns_front, columns_rear])
 
-interior1 = CUBOID([23,0.5])
+interior1 = CUBOID([23,0.5,H4-H3])
 interior1 = T([1,2,3])([12,20,H3int])(interior1)
-interior2 = CUBOID([2,3])
+interior2 = CUBOID([2,3,H4-H3])
 interior2 = T([1,2,3])([15,20.5,H3int])(interior2)
-interior3 = CUBOID([0.5,9])
+interior3 = CUBOID([0.5,9,H4-H3])
 interior3 = T([1,2,3])([31,20.5,H3int])(interior3)
 
 interior1b = T([2])([9])(interior1)
@@ -75,62 +75,43 @@ interior2b = T([2])([5.5])(interior2)
 
 interiorTOT = COLOR(OCRA3A)(STRUCT([interior1, interior2, interior3, interior1b, interior2b]))
 
-floor3 = STRUCT([floor3base,columnsTOT,interiorTOT])
+floor3_3d = STRUCT([columnsTOT,interiorTOT])
 
 #floor4: top of the columns (roof)
 floor4a = T([1,2])([2,2])((CUBOID([34,14])))
 floor4b = CUBOID([38,18])
 floor4 = DIFFERENCE([floor4b,floor4a])
-floor4 = T([1,2,3])([6,16,H4])(floor4)
+floor4 = T([1,2,3])([6,16,H4])(PROD([floor4, Q(1)]))
 
-floor4int = T([3])([H4-H3])(interiorTOT)
-floor4coltop = CUBOID([2,8.5])
-floor4coltopfront = T([1,2,3])([12,20.5,H4])(floor4coltop)
-floor4coltoprear = T([1,2,3])([33,20.5,H4])(floor4coltop)
-floor4 = COLOR(OCRA4)((STRUCT([floor4,floor4int,floor4coltopfront,floor4coltoprear])))
+#floor4int = T([3])([H4-H3])(interiorTOT)
+floor4coltop = CUBOID([2,9.5,1])
+floor4coltopfront = T([1,2,3])([12,20,H4])(floor4coltop)
+floor4coltoprear = T([1,2,3])([33,20,H4])(floor4coltop)
+floor4_3d = COLOR(OCRA4)((STRUCT([floor4,floor4coltopfront,floor4coltoprear])))
 
-#north plan
-northcols = CUBOID([0,1.6,H4-H3])
-northcols = T([1,2,3])([6,16.2,H3])(northcols)
-northcols = COLOR(OCRA5)(STRUCT([northcols, T(2)(3.2)]*6))
+npts = [[0,0,0],[0,18,0],[0,9,4],
+		[-2,0,0],[-2,18,0],[-2,9,4]]
 
-npts = [[0,0,0],[0,18,0],[0,9,4]]
 tri = JOIN(AA(MK)(npts))
-tri = COLOR(OCRA8)(T([1,2,3])([6,16,H4+3])(tri))
+tri = COLOR(OCRA8)(T([1,2,3])([8,16,H4+3])(tri))
 
-trave = CUBOID([0,18,2])
-trave = COLOR(OCRA7)(T([1,2,3])([6,16,H4+1])(trave))
+trave = CUBOID([-2,18,2])
+trave = COLOR(OCRA7)(T([1,2,3])([8,16,H4+1])(trave))
 
-ntravecols = CUBOID([0,18,1])
-ntravecols = COLOR(OCRA6)(T([1,2,3])([6,16,H4])(ntravecols))
-
-north = STRUCT([northcols,tri,trave,ntravecols])
+north = STRUCT([tri,trave])
 
 #south plan
-south = T([1])([38])(north)
+south = T([1])([36])(north)
 
-#west plan
-westcols = CUBOID([1.6,0,H4-H3])
-westcols = T([1,2,3])([6.2,16,H3])(westcols)
-westcols = COLOR(OCRA5)(STRUCT([westcols, T(1)(3.28)]*12))
-wtravecols = CUBOID([38,0,1])
-wtravecols = COLOR(OCRA6)(T([1,2,3])([6,16,H4])(wtravecols))
+#north south - together
+vertical = STRUCT([north, south])
 
-west = STRUCT([westcols,wtravecols])
-
-#east plan
-east = T([2])([18])(west)
-
-#north south east west - together
-vertical = STRUCT([north,south,west,east])
-
-#---------> aggiungere floor0 alla fine
-two_and_half_model = STRUCT([floor0_3d,floor1_3d,floor2_3d,floor3,floor4])
-mockup3d = STRUCT([two_and_half_model,vertical])
+two_and_half_model = STRUCT([floor0_3d,floor1_3d,floor2_3d,floor3_3d,floor4_3d])
+solidmodel3d = STRUCT([two_and_half_model,vertical])
 
 #final view
-VIEW(mockup3d)
-VIEW(SKELETON(1)(mockup3d))
+VIEW(solidmodel3d)
+VIEW(SKELETON(1)(solidmodel3d))
 
 
 exit()
