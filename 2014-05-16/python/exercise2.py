131a6b4d5ff9dec0c73c2089ead0c60b44a89904
davideviolante <fungtq3@gmail.com>
2014-05-25 18:55:32 +0200

    hw3 completed

--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -264,7 +264,17 @@ cornice = DIFFERENCE([cornice1,(roof1)])
 #maindoor fill
 mdfill = CUBOID([2,Ky+we,we])
 mdfill = T([1,2,3])([we+Lx+we,we+Ly+we+Ky+we+we,3.25])(mdfill)
-roof = STRUCT([cornice, roof, mdfill])
+
+cornice2 = CUBOID([0.5,7+wi,0.2])
+cornice2 = T([1,2,3])([-0.5,totY*2-7.15,3.2*7+0.25])(cornice2)
+cornice3 = CUBOID([5+we+0.5,0.5,0.2])
+cornice3 = T([1,2,3])([-0.5,totY*2,3.2*7+0.25])(cornice3)
+cornice23 = STRUCT([cornice2,cornice3])
+
+cornice45 = S([2,1])(-1)(cornice23)
+cornice45 = T([1,2])([0,totY*2])(cornice45)
+
+roof = STRUCT([cornice, roof, mdfill, cornice23, cornice45])
 
 # ----- BUILDING DOOR ----- #
 maindoor1 = CUBOID([0.5,1.5,2])
@@ -369,6 +379,14 @@ gwcorner2 = T([1,2])([totX,totY*2])(gwcorner2)
 gwcorner = DIFFERENCE([gwcorner2,gwcornerR])
 
 gw = STRUCT([gw1,gw2,gwcorner])
+
+gardoor1 = CUBOID([3.5,0.05,2])
+gardoor1 = T([1,2,3])([10,totY*2-wi,0.9])(gardoor1)
+gardoor2 = CUBOID([0.05,8,2])
+gardoor2 = T([1,2,3])([totX-0.1,totY+1.4,0.6])(gardoor2)
+
+gardoors = STRUCT([gardoor1,gardoor2])
+
 gars = STRUCT([gw,gs,ramps])
 
 # ----- GRASS ----- #
@@ -381,6 +399,23 @@ grass3 = JOIN(AA(MK)(ptsgrass))
 grass3 = PROD([grass3, Q(3.25)])
 
 grass = STRUCT([grass1,grass2,grass3])
+
+# ----- TREES ----- #
+BROWN1 = rgb([110, 65, 0])
+GREEN2 = rgb([0, 150, 0])
+trunk = COLOR(BROWN1)(CYLINDER([0.25,2])(10))
+foliage0 = T(3)(1.25)(CONE([1.5,2.5])(20))
+foliage1 = T(3)(2.75)(CONE([1,2.5])(20))
+foliage2 = T(3)(4.25)(CONE([0.67,2.5])(20))
+foliage = COLOR(GREEN2)(STRUCT([foliage0,foliage1,foliage2]))
+
+tree = STRUCT([trunk,foliage])
+tree = T([1,2,3])([3,totY*2+8,3])(tree)
+
+trees1 = STRUCT(NN(3)([tree,T([1])([6])]))
+trees2 = R([1,2])(PI/2)(trees1)
+trees2 = T([1,2])([55,0])(trees2)
+trees = STRUCT([trees1,trees2])
 
 # ----- STREET LAMPS ----- #
 slamp1 = CYLINDER([0.07,6])(12)
@@ -418,7 +453,11 @@ wglass6 = MATERIAL(glass_mat)(wglass6)
 
 wglass7 = T([2])([10.5])(wglass6)
 
-wglass = STRUCT([wglass1,wglass2,wglass3,wglass4,wglass5,wglass6,wglass7])
+mdglass1 = CUBOID([0.05,2,2])
+mdglass1 = T([1,2,3])([we+Lx+we-0.125,we+Ly+we+Ky+Ky/2,3.25])(mdglass1)
+mdglass1 = MATERIAL([0,0,0,1, 0,0,0,0.4, 0,0,0,1, 0,0,0,1, 100])(mdglass1)
+
+wglass = STRUCT([wglass1,wglass2,wglass3,wglass4,wglass5,wglass6,wglass7,mdglass1])
 
 # ----- CROSSWALKS ----- #
 crwalk1 = CUBOID([0.6,4.5,0.01])
@@ -437,11 +476,11 @@ roof = COLOR(OCRA1)(roof)
 street = COLOR(BLACK1)(street)
 sidewalk = COLOR(BLACK2)(sidewalk)
 gars = COLOR(BLACK1)(gars)
+gardoors = COLOR(GREY1)(gardoors)
 grass = COLOR(GREEN1)(grass)
 slamps = COLOR(GREY1)(slamps)
 
 totalview = STRUCT(totalview)
 totalview = COLOR(OCRA1)(totalview)
 
-
-VIEW(STRUCT([totalview, stairs, roof, street, sidewalk, gars, grass, slamps, crwalks, wglass]))
+VIEW(STRUCT([totalview, stairs, roof, street, sidewalk, gars, gardoors, grass, trees, slamps, crwalks, wglass]))
